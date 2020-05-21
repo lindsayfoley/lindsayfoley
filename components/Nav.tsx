07547f448eb86d0074e4c3c90daced1543c5bb87
Lindsay Foley <lindsay.foley@outlook.com>
2020-05-21 21:56:04 +0100

    fixed broken Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,7 +8,7 @@ interface INavProps {
 
 const Nav: React.FunctionComponent<INavProps> = ({ buttons }) =>
   <nav>
-    {buttons.map(button => <AnchorButton {...button} key={button[0].cta} />)}
+    {buttons.map((button, index) => <AnchorButton {...button} key={index} />)}
   </nav>;
 
 export default Nav;
