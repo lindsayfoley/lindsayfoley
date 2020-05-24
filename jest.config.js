c842b9b1a02a70ab6121b44f76e2a9eaa019f2c5
Lindsay Foley <lindsay.foley@outlook.com>
2020-05-24 14:51:54 +0100

    updates to enable tests and snapshots

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,7 @@ module.exports = {
     "^.+\\.(ts|tsx|js)?$": "ts-jest",
   },
   testMatch: ["**/*.(test|spec).(ts|tsx)"],
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"]
+  setupFilesAfterEnv: ["<rootDir>/public/__mocks__/setupTests.js"],
+  moduleNameMapper: { "^.+\\.(css|scss)$": "<rootDir>/public/__mocks__/SCSSStub.js" },
+  snapshotSerializers: ["enzyme-to-json/serializer"]
 };
