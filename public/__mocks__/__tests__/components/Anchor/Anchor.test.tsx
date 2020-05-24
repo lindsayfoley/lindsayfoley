c725a6829d7add3cf67a9926ed2a11987abc95cf
Lindsay Foley <lindsay.foley@outlook.com>
2020-05-24 14:52:51 +0100

    added tests and snaphots

---
+++ public/__mocks__/__tests__/components/Anchor/Anchor.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Anchor, { IAnchorProps } from '../../../../../components/Anchor';
+
+const defaultProps: Pick<IAnchorProps, 'link'> = {
+    link: 'test'
+}
+
+const setup = (props = {}) => {
+    const setupProps: IAnchorProps = { ...defaultProps, ...props }
+    return shallow(<Anchor {...setupProps} />);
+};
+
+describe('Anchor', () => {
+
+    it('renders correctly', () => {
+        const wrapper = setup();
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('only renders a link', () => {
+        const wrapper = setup();
+        expect(wrapper.find('[href]').length).toBe(1);
+        expect(wrapper.find('i').length).toBe(0);
+        expect(wrapper.find('[title]').length).toBe(0);
+        expect(wrapper.find('a').text()).toEqual('');
+    });
+
+    it('renders an icon', () => {
+        const wrapper = setup({ icon: 'fa fa-paper-plane' });
+        expect(wrapper.find('i').length).toBe(1);
+    });
+
+    it('adds a title attribute', () => {
+        const wrapper = setup({ title: 'test title' });
+        expect(wrapper.find('[title]').length).toBe(1);
+    });
+
+    it('displays a cta', () => {
+        const testCta = 'I am a call to action';
+        const wrapper = setup({ cta: testCta });
+        expect(wrapper.find('a').text()).toEqual(testCta);
+    });
+
+    it('adds target blank to links when external is true', () => {
+        const wrapper = setup({ external: true });
+        expect(wrapper.find('a').prop('target')).toEqual('_blank');
+    });
+});
---
+++ public/__mocks__/__tests__/components/Button/Button.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import Button, { IButtonProps } from '../../../../../components/Button';
+
+const defaultProps: IButtonProps = {
+    link: 'test link',
+    cta: 'test cta'
+}
+
+const setup = (props = {}) => {
+    const setupProps = {...defaultProps, ...props}
+    return shallow(<Button {...setupProps} />);
+}
+
+describe('Button', () => {
+
+    it('renders correctly', () => {
+        const wrapper = setup();
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('renders a button', () => {
+        const wrapper = setup();
+        expect(wrapper.find('button').length).toBe(1);
+        expect(wrapper.find('[href]').length).toBe(1);
+        expect(wrapper.prop('href')).toEqual(defaultProps.link);
+        expect(wrapper.find('button').text()).toEqual(defaultProps.cta);
+    });
+});
