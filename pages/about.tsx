51563b4058a50ee6273b41d9c175cb8de04f0bf8
Lindsay Foley <lindsay.foley@outlook.com>
2020-10-03 23:34:52 +0100

    updated skills, tweaked styling and removed component

--- pages/about.tsx
+++ pages/about.tsx
@@ -26,35 +26,28 @@ const AboutPage: React.FunctionComponent = () => {
           <div>
             <h3>My experience</h3>
             <Coding />
-            <p>I have 9 years of experience working in an Ecommerce environment and have been developing professionally for about 8 years now.</p>
-            <p>In that time, I've been fortunate to work for some notable companies including Oliver Bonas, Anya Hindmarch, Selfridges and Disney and most recently Charlotte Tilbury, where I joined as a Frontend Engineer working on their React site.</p>
+            <p>I have 10 years of experience working in Ecommerce and have been developing professionally for 9 years.</p>
+            <p>In that time, I've been very fortunate to work for notable companies including Oliver Bonas, Anya Hindmarch, Selfridges, Disney and most recently Charlotte Tilbury, where I work as a Frontend Engineer on their React site.</p>
           </div>
           <div>
             <dl>
               <dt>Web Development Skills</dt>
-              <dd>HTML</dd>
-              <dd>CSS, SCSS, BEM methodology</dd>
-              <dd>JavaScript ES6 standards</dd>
-              <dd>Typescript</dd>
-              <dd>React</dd>
-              <dd>Next.js/CRA frameworks</dd>
-              <dd>A bit of PHP &amp; VB</dd>
-              <dd>jQuery</dd>
-              <dd>SQL</dd>
-              <dd>Schema/Microdata</dd>
-              <dd>Git/SVN</dd>
+              <dd>HTML, CSS, SCSS, CSS-in-JS</dd>
+              <dd>Typescript, JavaScript, jQuery</dd>
+              <dd>React inc. hooks and Redux</dd>
+              <dd>Next.js &amp; CRA frameworks</dd>
+              <dd>Accessibility</dd>
+              <dd>Unit testing frameworks; Jest, Enzyme, Mocha</dd>
+              <dd>A bit of PHP, VB &amp; SQL</dd>
+              <dd>UI frameworks; Foundation &amp; Bootstrap</dd>
               <dd>A/B and MVT testing</dd>
-              <dd>Usability testing</dd>
-              <dd>Task automation e.g. Gulp</dd>
-              <dd>Foundation/Bootstrap</dd>
-              <dd>FE optimisation techniques</dd>
-              <dd>Load testing experience</dd>
-              <dd>Responsive HTML emails</dd>
-              <dd>Graphic and UI design</dd>
-              <dd>A11y best practises</dd>
+              <dd>Version control via Git &amp; SVN</dd>
+              <dd>Building responsive HTML emails</dd>
+              <dd>Tooling; Webpack, NPM, Babel, Gulp, Storybook</dd>
+              <dd>Vast CMS platforms; Contentful, Drupal, Wordpress, Squarespace</dd>
+              <dd>Graphic design, UI/UX design principles &amp; usability testing</dd>
               <dd>Qualified Digital Marketer</dd>
-              <dd>SEO experience</dd>
-              <dd>Agile methodologies</dd>
+              <dd>SEO best practises</dd>
             </dl>
           </div>
         </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Head from "next/head";
-import Intro from "../components/Intro";
 import Nav from "../components/Nav";
 import "../public/styles/_home.scss";
 
@@ -11,13 +10,19 @@ const IndexPage: React.FunctionComponent = () =>
       <title>I'm Lindsay Foley, A Front End Web Developer Based In London</title>
       <meta name="description" content="A Web Developer with many years of experience, specialising in front end development with a focus on design, UX, HTML, SCSS, Javascript and web optimisation." />
     </Head>
-    <header>
-      <h1 itemProp="name">
-        <a href="/">Lindsay Foley</a>
-      </h1>
-    </header>
     <section id="home">
-      <Intro />
+      <header>
+        <h1 itemProp="name">
+          <a href="/">Lindsay Foley</a>
+        </h1>
+      </header>
+      <h2 itemProp="description">
+        <span itemProp="jobTitle">Web Developer</span> based in <span itemProp="workLocation">London</span>
+      </h2>
+      <img
+        src="/images/hp_hero.png"
+        alt="A neat and tidy desk housing all the essentials including a cat mug full of english breakfast tea, biscuits and an iMac"
+      />
       <Nav buttonArr={[
         { link: "/portfolio", cta: "View my work" },
         { link: "/about", cta: "about me" }
