6299980fc2f72b95c2cabef1281860288d168ca4
Lindsay Foley <lindsay.foley@outlook.com>
2022-03-15 23:28:57 +0000

    removing dupe files

--- pages/_app 2.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from "next/app";
-import "../public/styles/global.scss";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-export default MyApp;
