d5241c42aef1acfac927e79f5fcbd74ee4b8bb15
Lindsay Foley <lindsay.foley@outlook.com>
2022-03-24 23:31:12 +0000

    adding more tests

---
+++ components/organisms/about-me/about-me.test.tsx
@@ -0,0 +1,43 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import AboutMe from "./about-me";
+
+describe("AboutMe", () => {
+  beforeEach(() => {
+    render(<AboutMe />);
+  });
+
+  it("Should render a header", () => {
+    expect(screen.getByRole("banner")).toBeVisible();
+    expect(
+      screen.getByRole("heading", {
+        level: 1,
+        name: "All About Me",
+      }),
+    ).toBeVisible();
+  });
+
+  it("Should render the header as a link", () => {
+    expect(
+      screen.getByRole("link", {
+        name: "All About Me",
+      }),
+    ).toBeVisible();
+  });
+
+  it("Should render a description", () => {
+    expect(screen.getByText(/thank you for stopping by/)).toBeVisible();
+  });
+
+  it("Should render the main content along with a list of skills", () => {
+    expect(screen.getByRole("main")).toBeVisible();
+    expect(screen.getByRole("term")).toBeVisible();
+    expect(screen.getAllByRole("definition")[0]).toBeVisible();
+  });
+
+  it("Should render the required nav links", () => {
+    expect(screen.getByRole("navigation")).toBeVisible();
+    expect(screen.getByRole("link", { name: "View my work" })).toBeVisible();
+    expect(screen.getByRole("link", { name: "back to home" })).toBeVisible();
+  });
+});
