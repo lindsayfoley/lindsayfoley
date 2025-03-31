bff61d38b3da45d015eac1df0f2a1c04e2816017
Lindsay Foley <lindsay.foley@outlook.com>
2025-03-31 17:16:28 +0100

    adding canonical tag and .htaccess to handle http and www redirects

--- components/templates/layout/layout.tsx
+++ components/templates/layout/layout.tsx
@@ -3,34 +3,43 @@ import Head from "next/head";
 import Script from "next/script";
 import { Footer } from "components/organisms";
 import { Logo } from "components/assets/svgs/logo";
+import { useRouter } from "next/dist/client/router";
 import styles from "./layout.module.scss";
 
-const Layout = ({ children }: { children: ReactNode }) => (
-  <>
-    <Head>
-      <meta
-        name="keywords"
-        content="freelancer, freelance web developer, frontend developer, frontend engineer, web development, London, React, TypeScript, JavaScript, SCSS, web design, interactive websites, ecommerce, Lindsay Foley"
+const Layout = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="keywords"
+          content="freelancer, freelance web developer, frontend developer, frontend engineer, web development, London, React, TypeScript, JavaScript, SCSS, web design, interactive websites, ecommerce, Lindsay Foley"
+        />
+        <link
+          rel="canonical"
+          href={`https://lindsayfoley.co.uk${router.asPath.split("?")[0]}`}
+        />
+      </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-69DWCVH1KC"
       />
-    </Head>
-    <Script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-69DWCVH1KC"
-    />
-    <Script
-      dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-69DWCVH1KC');`,
-      }}
-    />
-    <a href="/">
-      <Logo className={styles.logo} />
-    </a>
-    {children}
-    <Footer />
-  </>
-);
+        }}
+      />
+      <a href="/">
+        <Logo className={styles.logo} />
+      </a>
+      {children}
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
